4a49a3dc54ea57ef61d3b1705a4441d5b17fb13a
Mihai Spataru <mihaispataru@gmail.com>
2022-05-18 15:21:21 +0300

    add a simple in memory caching layer for authentication

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,16 +1,29 @@
 const { Profile } = require('../model');
 
-const authentication = async (req, res, next) => {
-  const profile = await Profile.findOne(
-    {
-      where: {
-        id: req.get('profile_id') || 0,
-      },
-    },
-  );
+const authenticationCache = new Map();
 
-  if (!profile) {
+const authentication = async (req, res, next) => {
+  const profileId = req.get('profile_id') || null;
+
+  if (profileId === null) {
     return res.status(401).end();
+  }
+
+  let profile = authenticationCache.get(profileId) || null;
+  if (profile === null) {
+    profile = await Profile.findOne(
+      {
+        where: {
+          id: profileId,
+        },
+      },
+    );
+
+    if (!profile) {
+      return res.status(401).end();
+    }
+
+    authenticationCache.set(profileId, profile);
   }
 
   req.app.set('profile', profile);
